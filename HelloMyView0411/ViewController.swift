d307df7866e5feb276f03d61e4cde04d854629dd
Danny Shen <shenfive@gmail.com>
2020-04-11 10:36:07 +0800

    network

--- HelloMyView0411/ViewController.swift
+++ HelloMyView0411/ViewController.swift
@@ -21,10 +21,19 @@ class ViewController: UIViewController {
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        var newView = MyView(frame: CGRect(x: 10, y: 10, width: 100, height: 200))
+        let newView = MyView(frame: CGRect(x: 10, y: 10, width: 100, height: 200))
         self.view.addSubview(newView)
         
-        
+        print(getNetworkStatus())
     }
+    
+    func getNetworkStatus()->Bool{
+        if Reachability(hostName: "www.apple.com")?.currentReachabilityStatus().rawValue == 0 {
+            return false
+        }else{
+            return true
+        }
+    }
+
 }
 
